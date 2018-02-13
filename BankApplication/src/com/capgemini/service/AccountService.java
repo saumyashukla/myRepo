97b228835e43da7b0a5b25d8ad8e10c58e32d498
Shukla <saumya.shukla@capgemini.com>
2018-02-13 12:49:12 +0530

    BankApp

---
+++ BankApplication/src/com/capgemini/service/AccountService.java
@@ -0,0 +1,12 @@
+package com.capgemini.service;
+
+import com.capgemini.exceptions.InsufficientInitialAmountException;
+import com.capgemini.exceptions.InvalidAccountNumberException;
+import com.capgemini.model.Account;
+
+public interface AccountService {
+
+	Account createAccount(int accountNumber, int amount) throws InsufficientInitialAmountException;
+	Account searchAccount(int accountNumber)throws InvalidAccountNumberException;
+
+}
